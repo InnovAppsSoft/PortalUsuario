b7d752564ea41179913105baba5d59be16308c0a
lesclaz <lesclaz95@gmail.com>
2023-11-02 21:34:04 -0400

    Migrate PrivacyActivity.java to PrivacyActivity.kt

--- app/src/main/java/com/marlon/portalusuario/activities/PrivacyActivity.kt
+++ app/src/main/java/com/marlon/portalusuario/activities/PrivacyActivity.kt
@@ -1,26 +1,16 @@
-package com.marlon.portalusuario.activities;
+package com.marlon.portalusuario.activities
 
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.marlon.portalusuario.databinding.ActivityPrivacyBinding
 
-import android.webkit.WebView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import com.marlon.portalusuario.R;
-
-
-public class PrivacyActivity extends AppCompatActivity {
-
-    Toolbar mToolbar;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_privacy);
-
-        WebView myWebView = findViewById(R.id.webview_privacy);
-        myWebView.loadUrl("https://portalusuarioapp.blogspot.com/p/portal-usuario-mas-que-una-simple.html");
-
+class PrivacyActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPrivacyBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPrivacyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.webviewPrivacy
+            .loadUrl("https://portalusuarioapp.blogspot.com/p/portal-usuario-mas-que-una-simple.html")
     }
 }
