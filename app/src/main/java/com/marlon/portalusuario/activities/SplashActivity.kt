af6eb84504bbd84ff9ab2e677f1c8bae94b8fe1b
lesclaz <lesclaz95@gmail.com>
2023-11-02 21:16:56 -0400

    Migrate SplashActivity.java to SplashActivity.kt

--- app/src/main/java/com/marlon/portalusuario/activities/SplashActivity.kt
+++ app/src/main/java/com/marlon/portalusuario/activities/SplashActivity.kt
@@ -1,45 +1,38 @@
-package com.marlon.portalusuario.activities;
+package com.marlon.portalusuario.activities
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.os.CountDownTimer;
+import android.content.Intent
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
+import com.marlon.portalusuario.R
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.preference.PreferenceManager;
+private const val CountDownInterval = 1000L
 
-import com.makeramen.roundedimageview.RoundedImageView;
-import com.marlon.portalusuario.R;
-
-public class SplashActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val settings = PreferenceManager.getDefaultSharedPreferences(
+            applicationContext
+        )
         // APP THEME
-        if (settings.getString("keynoche", "").equals("claro")) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        } else if (settings.getString("keynoche", "").equals("oscuro")) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        if (settings.getString("keynoche", "") == "claro") {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        } else if (settings.getString("keynoche", "") == "oscuro") {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         } else {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         }
-
-    new CountDownTimer(1000, 1000) {
-            @Override
-            public void onTick(long l) {
+        object : CountDownTimer(CountDownInterval, CountDownInterval) {
+            override fun onTick(l: Long) {
+                // Do nothing
             }
-
-            @Override
-            public void onFinish() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                finish();
+            override fun onFinish() {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
             }
-
-        }.start();
+        }.start()
     }
-
 }
