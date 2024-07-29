d1f2a4b1c400ee7747376886d79fb7dd99d09c23
lesclaz <lesclaz95@gmail.com>
2024-07-29 02:52:28 -0400

    fixes

--- feature/nauta-nav/build.gradle.kts
+++ feature/nauta-nav/build.gradle.kts
@@ -1,4 +1,4 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlin.android)
@@ -37,7 +37,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
+        kotlinCompilerExtensionVersion = "1.5.13"
     }
 }
 
