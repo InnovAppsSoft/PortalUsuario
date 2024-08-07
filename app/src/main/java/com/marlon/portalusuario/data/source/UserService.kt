2df39b7c0b79ce42eb9eac5d5142f88078c480dc
lesclaz <lesclaz95@gmail.com>
2024-08-07 08:12:09 -0400

    perf(mobServices): optimizado getUsers en UserService
    - se añade un limite de 10 intentos para obtener los servicios móviles de la cuenta
    - se remplaza la llamada recursiva de la función por un bucle while

--- app/src/main/java/com/marlon/portalusuario/data/source/UserService.kt
+++ app/src/main/java/com/marlon/portalusuario/data/source/UserService.kt
@@ -7,34 +7,41 @@ import com.marlon.portalusuario.domain.model.MobileService
 import com.marlon.portalusuario.domain.model.NavigationService
 import com.marlon.portalusuario.exceptions.SessionException
 import com.marlon.portalusuario.util.Utils.createPasswordApp
+import io.github.suitetecsa.sdk.exception.NautaException
 import io.github.suitetecsa.sdk.nauta.api.NautaService
 import io.github.suitetecsa.sdk.nauta.model.User
 import io.github.suitetecsa.sdk.nauta.model.users.UsersRequest
 
 private const val TAG = "UserService"
+private const val MaxIterations = 10
 
 class UserService(private val service: NautaService) {
-    suspend fun getUser(authorization: String, userRequest: UsersRequest): ResponseUser =
-        service.users("Bearer $authorization", userRequest, createPasswordApp()).let { response ->
-            Log.d(TAG, "getUser: $response")
-            if (response.result != "ok") throw SessionException(response.result)
-            (response.user as User).let { user ->
+    suspend fun getUser(authorization: String, userRequest: UsersRequest): ResponseUser {
+        var request = userRequest
+        var iterations = 0
+
+        while (iterations < MaxIterations) {
+            iterations++
+            service.users("Bearer $authorization", request, createPasswordApp()).let { response ->
+                if (response.result != "ok") throw SessionException(response.result)
+                val user = response.user as User
+                Log.d(TAG, "getUser: ${user.services.mobileServices}")
                 if (user.completed == "true") {
-                    ResponseUser(
-                        user.asProfile(),
-                        user.services.mobileServices.map { it.asModel() },
-                        user.services.navServices.map { it.asModel() }
-                    )
+                    return buildResponse(user)
                 } else {
-                    Log.d(
-                        TAG,
-                        "fetchUser: getting user with data: authToken:" +
-                            " $authorization, request: ${userRequest.copy(lastUpdate = user.lastUpdate)}"
-                    )
-                    getUser(authorization, userRequest.copy(lastUpdate = user.lastUpdate))
+                    request = userRequest.copy(lastUpdate = user.lastUpdate)
                 }
             }
         }
+
+        throw NautaException("Error obteniendo los datos :: Máximo número de intentos alcanzado sin respuesta")
+    }
+
+    private fun buildResponse(user: User) = ResponseUser(
+        user.asProfile(),
+        user.services.mobileServices.map { it.asModel() },
+        user.services.navServices.map { it.asModel() }
+    )
 }
 
 data class ResponseUser(
