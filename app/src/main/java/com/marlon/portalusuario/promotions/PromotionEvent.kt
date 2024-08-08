5a91e7936b82173e8596150f24e3b125cf87bac7
lesclaz <lesclaz95@gmail.com>
2024-08-08 14:25:58 -0400

    fix(build): elimina el banner de promociones de etecsa
    se elimina el banner de promociones de etecsa porque la dependencia ya no existe.

--- app/src/main/java/com/marlon/portalusuario/promotions/PromotionEvent.kt
+++
@@ -1,5 +0,0 @@
-package com.marlon.portalusuario.promotions
-
-sealed class PromotionEvent {
-    data object Load : PromotionEvent()
-}
--- app/src/main/java/com/marlon/portalusuario/promotions/model/Promotion.kt
+++
@@ -1,3 +0,0 @@
-package com.marlon.portalusuario.promotions.model
-
-class Promotion(val svg: String, val background: String, var url: String)
